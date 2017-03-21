40fe43037c77281bad0fb5dd44d3500e164d2bc7
Sikimic <sikimic94@gmail.com>
2017-03-21 20:36:54 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 1337;
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.get('/', function (req, res)  { 
+	res.status(200).send('Hello World'); 
+});
+
+app.listen(port, function() {
+	console.log('listening on port ' + port);
+});
+
+app.post('/hello', function(req, res, next) {
+	var username = req.body.user_name;
+	var botPayoad = {
+		text: 'Hello' + username + ', welcome tasdfads'
+	};
+
+	if (username !== 'slackbot') {
+		return res.status(200).json(botPayoad);
+	} else {
+		return res.status(200).end();
+	}
+
+});
