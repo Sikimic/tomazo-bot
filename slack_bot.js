8190101bbe0c0e3c0e94d90e1ffd34fcbe22dad3
Nebojsa Sikimic <sikimic@Unkindled.local>
2017-04-11 00:28:31 +0200

    Add tomaze response

--- slack_bot.js
+++ slack_bot.js
@@ -90,13 +90,14 @@ controller.hears(initiateConversationStrings, 'direct_message,direct_mention,men
     if (!err) {
       convo.say('Ooooo');
 
-      var firstSentance = firstSentanceStrings[ Math.random() * firstSentanceStrings.length ];
-      var secondSentance = secondSentanceStrings[ Math.random() * secondSentanceStrings.length ];
+      var firstSentance = firstSentanceStrings[ Math.floor(Math.random() * firstSentanceStrings.length) ];
+      var secondSentance = secondSentanceStrings[ Math.floor(Math.random() * secondSentanceStrings.length) ];
 
       convo.ask(firstSentance + " " + secondSentance, function(response, convo) {
         bot.say("Vazi, vazi");
-        var finalSentance = finalSentanceStrings[ Math.random() * finalSentanceStrings.length ];
+        var finalSentance = finalSentanceStrings[ Math.floor(Math.random() * finalSentanceStrings.length) ];
         bot.say(finalSentance);
+        convo.next();
       });
     }
   });
